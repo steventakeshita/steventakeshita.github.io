db9fe786e6c3aea86f51af9e42e0e5c5be4a97af
steventakeshita <steven.hiroshi@gmail.com>
2021-04-06 14:08:10 -1000

    adding new graphics

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -1,7 +1,7 @@
 import * as S from "./styles";
 
-const Button = ({ color, width, children, onClick }) => (
-  <S.Button color={color} width={width} onClick={onClick}>
+const Button = ({ color, width, children, onClick, disabled }) => (
+  <S.Button color={color} width={width} onClick={onClick} disabled={disabled}>
     {children}
   </S.Button>
 );
